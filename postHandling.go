ad56b81ac4f2037797563d1192732f54f765d72d
Luminarys <kizunanohikari@gmail.com>
2015-06-28 01:45:01 -0500

    Minor improvements

--- postHandling.go
+++ postHandling.go
@@ -21,32 +21,36 @@ type jsonResponse struct {
 	Files   []jsonFile `json:"files"`
 }
 
+func throwErr(arr *[]jsonFile) string{
+    jsonResp := &jsonResponse{Success: false, Files: *arr}
+	jresp, err := json.Marshal(jsonResp)
+	if err != nil {
+	    fmt.Println(err.Error())
+	}
+	return string(jresp)
+}
+
 //Handles POST upload requests. the updateURL is used to pass messages
 //to the urlHandler indicating that the DB should be updated.
 func handleUpload(ctx *web.Context, updateURL chan<- string, updateResp <-chan *Response) string {
 	//TODO: Implemente limits with settings.ini or something
-	//TODO: Verify the max number of files that pomf could upload
 	err := ctx.Request.ParseMultipartForm(500 * 1024 * 1024)
 	if err != nil {
 		return "Error handling form!\n"
 	}
 	form := ctx.Request.MultipartForm
+
 	//Loop through and append to the response struct
-    c := 0
-	for _, _ = range form.File["files[]"] {
-        c++
-    }
-    fmt.Println(c)
-	resFiles := make([]jsonFile, c)
+	resFiles := make([]jsonFile, len(form.File["files[]"]))
 	for idx, fileHeader := range form.File["files[]"] {
 		filename := fileHeader.Filename
 		file, err := fileHeader.Open()
 		size, err := file.Seek(0, 2)
 		if err != nil {
-			return "Error parsing file!\n"
+            return throwErr(&resFiles)
 		}
 		if size > 50*1024*1024 {
-			return "File too big!\n"
+            return throwErr(&resFiles)
 		}
 		//Seek back to beginning
 		file.Seek(0, 0)
@@ -54,33 +58,32 @@ func handleUpload(ctx *web.Context, updateURL chan<- string, updateResp <-chan *
 			return err.Error()
 		}
 		hash := Md5(file)
-		//If file doesn't already exist, create it
-		if _, err := os.Stat("files/" + hash); os.IsNotExist(err) {
-			f, err := os.Create("files/" + hash)
-			if err != nil {
-				return "Error, file could not be created.\n"
-			}
-			_, err = file.Seek(0, 0)
-			if err != nil {
-				return "Error reading the file\n"
-			}
-			_, err = io.Copy(f, file)
-			if err != nil {
-				return "Error, file could not be written to.\n"
-			}
-		}
-
 		ext := filepath.Ext(filename)
 		//Send the hash and ext for updating
 		updateURL <- ext + ":" + hash
 		resp := <-updateResp
 		//Even though this is redundant, it might eventually be useful
 		if resp.status == "Failure" {
-			return resp.message + "\n"
+            return throwErr(&resFiles)
 		} else {
 			jFile := jsonFile{Hash: hash, Name: filename, URL: resp.message, Size: int(size)}
-            fmt.Println("About to append to array")
 			resFiles[idx] = jFile
+		}
+
+		//If file doesn't already exist, create it
+		if _, err := os.Stat("files/" + hash); os.IsNotExist(err) {
+			f, err := os.Create("files/" + hash)
+			if err != nil {
+                return throwErr(&resFiles)
+			}
+			_, err = file.Seek(0, 0)
+			if err != nil {
+                return throwErr(&resFiles)
+			}
+			_, err = io.Copy(f, file)
+			if err != nil {
+                return throwErr(&resFiles)
+			}
 		}
 	}
 	jsonResp := &jsonResponse{Success: true, Files: resFiles}
