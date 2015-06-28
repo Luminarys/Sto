54a1a1c790590a8d024f5b07bbc2ddd6f0f64553
Luminarys <kizunanohikari@gmail.com>
2015-06-28 02:01:11 -0500

    Added in Chinese Cartoons

--- static/js/Sto.js
+++ static/js/Sto.js
@@ -1389,3 +1389,10 @@ $(function () {
 		up.upload()
 	})
 })
+//Set the grill
+$(document).ready(function() {
+    chinese_cartoon_pics = ["2.png", "3.png", "4.png", "5.png", "6.png", "7.png", "8.png", "9.png", "10.png", ]
+    var grill = "/img/" + chinese_cartoon_pics[Math.floor(Math.random()*chinese_cartoon_pics.length)];
+    $("body").css("background-image", "url(" + grill + "), url(/img/bg.png/");
+
+});
